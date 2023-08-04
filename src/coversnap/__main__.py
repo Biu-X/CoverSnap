da216652a3499ee65657182d9b2fd812cfc2ec6a
tohru <65994850+Tohrusky@users.noreply.github.com>
2023-08-04 15:58:12 +0800

    update main

--- src/coversnap/__main__.py
+++ src/coversnap/__main__.py
@@ -1,13 +1,14 @@
 import argparse
 import os
 import cv2
+import numpy as np
 from coversnap import capture_image
 
 
 def main() -> None:
     # parse args
     parser = argparse.ArgumentParser()
-    parser.description = ''
+    parser.description = 'Capture image from video and save it to file, return black image if failed'
     parser.add_argument(
         '-i', '--INPUT', help='absolute path to input video', type=str, required=True)
     parser.add_argument(
@@ -18,9 +19,13 @@ def main() -> None:
     file_extension = file_extension.lower()
     if file_extension not in ['.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff', '.gif',
                               '.pbm', '.pgm', '.ppm', '.webp', '.hdr', '.pfm', '.exr']:
-        raise ValueError(f'Unsupported file extension: {file_extension}')
+        print(f'Maybe unsupported file extension: {file_extension}')
 
     img = capture_image(args.INPUT)
+
+    if img is None:
+        print('Failed to capture image, using black image instead')
+        img = np.zeros((512, 512, 3), dtype=np.uint8)
 
     cv2.imencode(file_extension, img)[1].tofile(args.OUTPUT)
 
