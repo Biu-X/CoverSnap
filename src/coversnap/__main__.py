e0c6aab25f809366b73c88109df425a219549b5a
tohru <65994850+Tohrusky@users.noreply.github.com>
2023-08-04 15:34:09 +0800

    update main

--- src/coversnap/__main__.py
+++ src/coversnap/__main__.py
@@ -1,10 +1,29 @@
+import argparse
+import os
 import cv2
+from coversnap import capture_image
 
 
 def main() -> None:
-    print('Hello, world!')
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.description = ''
+    parser.add_argument(
+        '-i', '--INPUT', help='absolute path to input video', type=str, required=True)
+    parser.add_argument(
+        '-o', '--OUTPUT', help='absolute path to output image', type=str, required=True)
+    args = parser.parse_args()
+
+    _, file_extension = os.path.splitext(args.OUTPUT)
+    file_extension = file_extension.lower()
+    if file_extension not in ['.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff', '.gif',
+                              '.pbm', '.pgm', '.ppm', '.webp', '.hdr', '.pfm', '.exr']:
+        raise ValueError(f'Unsupported file extension: {file_extension}')
+
+    img = capture_image(args.INPUT)
+
+    cv2.imencode(file_extension, img)[1].tofile(args.OUTPUT)
 
 
 if __name__ == '__main__':
     main()
-    print(cv2.haveOpenVX())
